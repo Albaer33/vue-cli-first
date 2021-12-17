7104f15a0436020fffea549041a6c283e70804a5
Albaer33 <alisi3396@gmail.com>
2021-12-17 15:51:13 +0100

    esercizio finito

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <img src="../assets/img/custom-banner.jpg" alt="banner">
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Title",
+    };
+</script>
+
+<style scoped>
+    img {
+        margin-top: 50px;
+    }
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Title",
+    };
+</script>
+
+<style scoped>
+    h1 {
+        color: goldenrod;
+        font-size: 30px;
+    }
+</style>
